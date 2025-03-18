68464f3a7879f7fd2c468ecc6e6921dc4f0010e6
AlexanderChoy <sllevin.jr@gmail.com>
2025-03-19 00:48:04 +0300

    Improved code: union 2 loops in one for sum digits & last element of list to find Max after sorting list

--- Digits.java
+++ Digits.java
@@ -22,29 +22,35 @@ public class Digits {
 
 // вычисление суммы цифр
         int sum = 0;
-        for (int digit : digits) {
-            sum += digit; // Добавляем текущий элемент к сумме
-        }
-        System.out.println("Сумма цифр: " + sum);
-
-// вычисление суммы чётных чисел
         int sum2 = 0;
         for (int digit : digits) {
+            sum += digit; // Добавляем текущий элемент к сумме
             if (digit % 2 != 0) {
                 sum2 += digit;
             }
         }
+        System.out.println("Сумма цифр: " + sum);
         System.out.println("Сумма нечётных цифр: " + sum2);
 
+// вычисление суммы чётных чисел
+
+//        for (int digit : digits) {
+//            if (digit % 2 != 0) {
+//                sum2 += digit;
+//            }
+//        }
+//        System.out.println("Сумма нечётных цифр: " + sum2);
+
 // вычисление максимальной цифры
-        int max = digits.get(0); // Предполагаем, что первый элемент - максимальный
-        // Перебираем все элементы в ArrayList
-        for (int digit : digits) {
-            if (digit > max) {
-                max = digit; // Обновляем max, если найдено большее значение
-            }
-        }
-        System.out.println("Максимальная цифра числа: " + max);
+//        int max = digits.get(0); // Предполагаем, что первый элемент - максимальный
+//        // Перебираем все элементы в ArrayList
+//        for (int digit : digits) {
+//            if (digit > max) {
+//                max = digit; // Обновляем max, если найдено большее значение
+//            }
+//        }
+//        System.out.println("Максимальная цифра числа: " + max);
+        System.out.println("Максимальная цифра числа: " + digits.get(digits.size() - 1));
     }
 }
 
